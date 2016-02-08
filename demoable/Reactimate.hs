5a2cdc7404a62ab1a8ef376ddbe94f09ca32e73c
Alfred Xiao <xiaoyf@gmail.com>
2016-02-08 21:40:58 +1100

    added files for reactive animation

---
+++ demoable/Reactimate.hs
@@ -0,0 +1,64 @@
+module Reactimate where
+import Fal hiding (reactimate, sample, windowUser, makeStream, w32ToTime)
+import Graphics.SOE hiding (Region, Event)
+import qualified Graphics.SOE as G (Region, Event)
+import Draw (xWin,yWin,intToFloat)
+-- import Word (word32ToInt)
+import Control.Concurrent
+
+-- < reactimate :: String -> Behavior a -> (a -> IO Graphic) -> IO ()
+
+-- < windowUser :: Window -> IO (([Maybe UserAction],[Time]), IO ())
+
+-- | ((us,ts),addEvents) <- windowUser w
+
+sample :: Event ()
+sample = Event (\(us,_) -> map aux us)
+  where aux Nothing  = Just ()
+        aux (Just _) = Nothing
+
+-- | sample `snapshot_` franProg
+
+reactimate
+  :: String -> Behavior a -> (a -> IO Graphic) -> IO ()
+reactimate title franProg toGraphic
+  = runGraphics $
+    do w <- openWindowEx title (Just (0,0)) (Just (xWin,yWin)) drawBufferedGraphic (Just 30)
+       (user, addEvents) <- windowUser w
+       addEvents
+       let drawPic (Just p) =
+             do g <- toGraphic p
+                setGraphic w g
+                addEvents
+           drawPic Nothing  = return ()
+       mapM_ drawPic
+             (runEvent (sample `snapshot_` franProg) user)
+
+runEvent (Event fe) u = fe u
+
+makeStream :: IO ([a], a -> IO ())
+
+makeStream
+  = do ch <- newChan
+       contents <- getChanContents ch
+       return (contents, writeChan ch)
+
+windowUser
+  :: Window -> IO (([Maybe UserAction],[Time]), IO ())
+windowUser w
+  = do (evs, addEv) <- makeStream
+       t0 <- timeGetTime
+       let loop rt =
+             do mev <- maybeGetWindowEvent w
+                case mev of
+                  Nothing -> return ()
+                  Just e  -> do addEv (Just e, rt)
+                                loop rt
+       let addEvents =
+             do t <- timeGetTime
+                let rt = w32ToTime (t-t0)
+                loop rt
+                addEv (Nothing, rt)
+       return ((map fst evs, map snd evs), addEvents)
+
+w32ToTime t = intToFloat (fromInteger (toInteger t)) / 1000
